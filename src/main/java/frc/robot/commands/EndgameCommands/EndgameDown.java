efecd02ddd1c01958d8ff70dc55de33f95007153
jazzlike06 <eneskeles0619@gmail.com>
2025-01-27 12:56:14 +0300

    Changed m_endgame declaration

--- src/main/java/frc/robot/commands/EndgameCommands/EndgameDown.java
+++ src/main/java/frc/robot/commands/EndgameCommands/EndgameDown.java
@@ -2,19 +2,22 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.SwerveDrive.EndgameCommands;
+package frc.robot.commands.EndgameCommands;
 
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.Endgame.endgame;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class EndgameDown extends Command {
+  endgame m_endgame;
+
   /** Creates a new EndgameDown. */
-  public EndgameDown(endgame m_endgame) {
-    addRequirements(m_endgame);
+  public EndgameDown(endgame endgame) {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_endgame);
+
+    this.m_endgame = endgame;
   }
 
   // Called when the command is initially scheduled.
@@ -24,13 +27,13 @@ public class EndgameDown extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    RobotContainer.m_endgame.setSpeed(-0.5);
+    m_endgame.setSpeed(-0.5);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    RobotContainer.m_endgame.setSpeedZero();
+    m_endgame.setSpeedZero();
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/commands/EndgameCommands/EndgameUp.java
+++ src/main/java/frc/robot/commands/EndgameCommands/EndgameUp.java
@@ -2,18 +2,21 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.SwerveDrive.EndgameCommands;
+package frc.robot.commands.EndgameCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.Endgame.endgame;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class EndgameUp extends Command {
+  endgame m_endgame;
+
   /** Creates a new EndgameUp. */
-  public EndgameUp(endgame m_endgame) {
-    addRequirements(m_endgame);
+  public EndgameUp(endgame endgame) {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(endgame);
+
+    this.m_endgame = endgame;
   }
 
   // Called when the command is initially scheduled.
@@ -23,13 +26,13 @@ public class EndgameUp extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    RobotContainer.m_endgame.setSpeed(0.5);
+    m_endgame.setSpeed(0.5);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    RobotContainer.m_endgame.setSpeedZero();
+    m_endgame.setSpeedZero();
   }
 
   // Returns true when the command should end.
