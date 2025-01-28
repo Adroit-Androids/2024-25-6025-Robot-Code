d583f7fd99d5ea332cf432f0744969616dc76328
AdroitAndroids6025 <adroitandroids@gmail.com>
2025-01-28 14:54:55 +0300

    Reapply "Merge branch 'main' of https://github.com/jazzlike06/2024-25-6025-Robot-Code"
    This reverts commit 42c4f7641bf334bc0f3e308f6363a6e99139e294.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,22 +15,23 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int kOperatorControllerPort = 1;
 
     public static final double kLeftJoystickDeadband = 0.1;
     public static final double kRightJoystickDeadband = 0.1;
   }
 
   public static class MotorIds {
-    public static final int kCoralIntakeMotor = 1;
-    public static final int kCoralWristMotor = 2;
+    public static final int kCoralIntakeMotor = 10;
+    public static final int kCoralWristMotor = 10;
 
-    public static final int kAlgeaIntakeMotor = 3;
+    public static final int kAlgeaIntakeMotor = 11;
 
-    public static final int kElevatorLeadMotor = 4;
-    public static final int kElevatorFollowMotor = 5;
+    public static final int kElevatorLeadMotor = 15;
+    public static final int kElevatorFollowMotor = 16;
 
-    public static final int kEndgameLeftMotor = 6;
-    public static final int kEndgameRightMotor = 7;
+    public static final int kEndgameLeftMotor = 20;
+    public static final int kEndgameRightMotor = 21;
   }
 
 }
