e6978f158d82419d16655a6f4ae3b1b42c4980a5
AdroitAndroids6025 <adroitandroids@gmail.com>
2025-02-04 13:07:11 +0300

    Removed redundancy

--- src/main/java/frc/robot/commands/SwerveDrive/apriltagDistance.java
+++ src/main/java/frc/robot/commands/SwerveDrive/apriltagDistance.java
@@ -5,7 +5,6 @@
 package frc.robot.commands.SwerveDrive;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Limelight.Limelight;
--- src/main/java/frc/robot/subsystems/Limelight/limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight/limelight.java
@@ -4,7 +4,6 @@
 
 package frc.robot.subsystems.Limelight;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.LimelightHelpers;
