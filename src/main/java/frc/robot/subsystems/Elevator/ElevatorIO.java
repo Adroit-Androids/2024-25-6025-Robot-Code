f4646f0a9f72a34b9b8f54fe5d289d3dd3cec916
AdroitAndroids6025 <adroitandroids@gmail.com>
2025-02-13 13:46:29 +0300

    Update ElevatorIO.java

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorIO.java
@@ -8,25 +8,32 @@ package frc.robot.subsystems.Elevator;
     
     @Autolog
     public class ElevatorIOInputs{
-      public boolean elevatorMotorConnected = true;
-      public boolean elevatorMotorFollowerConnected = true;
+    // Inputs for the elevator
+      public boolean motorLeaderConnected = true;
+      public boolean motorFollowerConnected = true;
+
       public double motorCurrent = 0.0;
       public double voltageCurent = 0.0;
       public double elevatorPositionRad = 0.0;
       public double elevatorVelocity = 0.0;
     }
     
+    // Set the current position of the elevator
     public default double setPosition(double position) {}
 
+    // Set the voltage of the elevator
     public default void setVoltage(double voltage) {}
 
+    // Reset the position of the elevator
     public default void resetPosition() {}
-    
+
+    // Update the inputs for the elevator
     public default void updateInputs(ElevatorIOInputs inputs) {}
 
+    // Get the current velocity of the elevator
     public default double getVelocity() {
       return 0;
     }
 
-    public default void setProfieldPIDValues(double kP, double kI, double kD) {}
+    public default void setProfiledPIDValues(double kP, double kI, double kD) {}
   }
